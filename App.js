e5ce05d82447b5545fe91a44e6b11e47dffd1755
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-09 08:36:55 +0800

    some files have been added

--- App.js
+++ App.js
@@ -5,16 +5,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Future software</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = 
+StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
